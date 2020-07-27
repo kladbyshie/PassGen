4a40eb9048777ac0a6e9f9d86605655e924dbe7a
ngaragulya <ngaragulya@gmail.com>
2020-07-27 16:15:45 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace PassGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nameField = new System.Windows.Forms.TextBox();
+            this.seedField = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.seedLabel = new System.Windows.Forms.Label();
+            this.passField = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.charField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameField
+            // 
+            this.nameField.Location = new System.Drawing.Point(42, 47);
+            this.nameField.Name = "nameField";
+            this.nameField.Size = new System.Drawing.Size(171, 27);
+            this.nameField.TabIndex = 0;
+            this.nameField.TextChanged += new System.EventHandler(this.nameField_TextChanged);
+            // 
+            // seedField
+            // 
+            this.seedField.Location = new System.Drawing.Point(219, 47);
+            this.seedField.Name = "seedField";
+            this.seedField.Size = new System.Drawing.Size(102, 27);
+            this.seedField.TabIndex = 0;
+            this.seedField.TextChanged += new System.EventHandler(this.seedField_TextChanged);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(42, 24);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(49, 20);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name";
+            // 
+            // seedLabel
+            // 
+            this.seedLabel.AutoSize = true;
+            this.seedLabel.Location = new System.Drawing.Point(219, 24);
+            this.seedLabel.Name = "seedLabel";
+            this.seedLabel.Size = new System.Drawing.Size(42, 20);
+            this.seedLabel.TabIndex = 1;
+            this.seedLabel.Text = "Seed";
+            // 
+            // passField
+            // 
+            this.passField.Location = new System.Drawing.Point(42, 115);
+            this.passField.Name = "passField";
+            this.passField.Size = new System.Drawing.Size(373, 27);
+            this.passField.TabIndex = 0;
+            this.passField.TextChanged += new System.EventHandler(this.passField_TextChanged);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(42, 92);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(70, 20);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(442, 47);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(94, 95);
+            this.generateButton.TabIndex = 3;
+            this.generateButton.Text = "Generate!";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // charField
+            // 
+            this.charField.Location = new System.Drawing.Point(327, 47);
+            this.charField.Name = "charField";
+            this.charField.Size = new System.Drawing.Size(88, 27);
+            this.charField.TabIndex = 0;
+            this.charField.TextChanged += new System.EventHandler(this.charField_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(327, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "# of Chars";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 180);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.charField);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.passField);
+            this.Controls.Add(this.seedLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.seedField);
+            this.Controls.Add(this.nameField);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PassGen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameField;
+        private System.Windows.Forms.TextBox seedField;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label seedLabel;
+        private System.Windows.Forms.TextBox passField;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.TextBox charField;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string name = String.Empty;
+        private string seed = String.Empty;
+        private string charcount = String.Empty;
+
+        public string CoreName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Seed
+        {
+            get { return seed; }
+            set { seed = value; }
+        }
+        public string CharCount
+        {
+            get { return charcount; }
+            set { charcount = value; }
+        }
+
+        public void PassGen(string CoreName = "test", string Seed = "Test2", string CharCount = "10")
+        {
+            int newpass = 0;
+            char[] chararray = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?".ToArray();
+            foreach(char c in CoreName)
+            {
+                int a = Convert.ToInt32(c);
+                newpass += a;
+            }
+            foreach (char d in Seed)
+            {
+                int b = Convert.ToInt32(d);
+                newpass += b;
+            }
+            Random rand = new Random(newpass);
+            string password = String.Empty;
+            for (int e = 0; e < Convert.ToInt32(CharCount); e++)
+            {
+                int a = rand.Next(0, chararray.Length);
+                char ch = chararray[a];
+                password += ch;
+            }
+            
+            passField.Text = password;
+        }
+
+        private void nameField_TextChanged(object sender, EventArgs e)
+        {
+            CoreName = nameField.Text;
+        }
+
+        private void seedField_TextChanged(object sender, EventArgs e)
+        {
+            Seed = seedField.Text;
+        }
+
+        private void passField_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void charField_TextChanged(object sender, EventArgs e)
+        {
+            CharCount = charField.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            seedField.Text = "DefaultSeed";
+            charField.Text = "10";
+            nameField.Select();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            passField.Text = String.Empty;
+
+            if (CoreName.Length > 0 || Seed.Length > 0 || CharCount.Length > 0)
+            {
+                PassGen(CoreName, Seed, CharCount);
+            }
+        }
+
+    }
+}
