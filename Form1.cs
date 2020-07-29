3d5f894d20079c8688cacde5c611d89e1852e7e3
ngaragulya <ngaragulya@gmail.com>
2020-07-29 16:07:41 -0400

    updated random gen and added shuffle

--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,32 +19,19 @@ namespace PassGen
             InitializeComponent();
         }
 
-        private string name = String.Empty;
-        private string seed = String.Empty;
-        private string charcount = String.Empty;
+        public string CoreName { get; set; }
+        public string Seed { get; set; }
+        public string CharCount { get; set; }
 
-        public string CoreName
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public string Seed
-        {
-            get { return seed; }
-            set { seed = value; }
-        }
-        public string CharCount
-        {
-            get { return charcount; }
-            set { charcount = value; }
-        }
-
-        public void PassGen(string CoreName = "test", string Seed = "Test2", string CharCount = "10")
+        public void PassGen(string CoreName = "Default", string Seed = "Default", string charcount = "10")
         {
             int newpass = 0;
-            char[] chararray = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".ToArray();
-            foreach(char c in CoreName)
+            int CharCount = Convert.ToInt32(charcount);
+            char[] lowercasearray = "abcdefghijklmnopqrstuvwxyz".ToArray();
+            char[] uppercasearray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToArray();
+            char[] numberarray = "0123456789".ToArray();
+            char[] specialarray = "!?".ToArray();
+            foreach (char c in CoreName)
             {
                 int a = Convert.ToInt32(c);
                 newpass += a;
@@ -52,16 +41,63 @@ namespace PassGen
                 int b = Convert.ToInt32(d);
                 newpass += b;
             }
-            //This creates a new seed from the combination of the core name and the seed value and creates a random object.
+            //This creates a new seed from the combination of the core name and the seed value and creates a random object
             Random rand = new Random(newpass);
             string password = String.Empty;
-            for (int e = 0; e < Convert.ToInt32(CharCount); e++)
+            int ModifiedCharCount = CharCount;
+
+            //Creates random random distribution that will always add up to CharCount and have at least 1 of each type of char
+            var AllTotals = new List<int>();
+            for (int i = 0; i < 3; i++)
+                {
+                int num = rand.Next(1, ModifiedCharCount - (3 - i));
+                ModifiedCharCount -= num;
+                AllTotals.Add(num);
+                }
+            AllTotals.Add(ModifiedCharCount);
+
+            int upperquant = AllTotals[0];
+            int lowerquant = AllTotals[1];
+            int numquant = AllTotals[2];
+            int specialquant = AllTotals[3];
+
+            for (int e = 0; e < lowerquant; e++)
             {
-                int a = rand.Next(0, chararray.Length);
-                char ch = chararray[a];
+                int a = rand.Next(0, lowercasearray.Length);
+                char ch = lowercasearray[a];
                 password += ch;
             }
-            
+            for (int e = 0; e < upperquant; e++)
+            {
+                int a = rand.Next(0, uppercasearray.Length);
+                char ch = uppercasearray[a];
+                password += ch;
+            }
+            for (int e = 0; e < numquant; e++)
+            {
+                int a = rand.Next(0, numberarray.Length);
+                char ch = numberarray[a];
+                password += ch;
+            }
+            for (int e = 0; e < specialquant; e++)
+            {
+                int a = rand.Next(0, specialarray.Length);
+                char ch = specialarray[a];
+                password += ch;
+            }
+
+            //Shuffles the chars around within the string
+            char[] NewPassword = password.ToCharArray();
+            for (int i = 0; i < NewPassword.Length; i++)
+            {
+                int j = rand.Next(i, NewPassword.Length); 
+                char temp = NewPassword[i];
+                NewPassword[i] = NewPassword[j];
+                NewPassword[j] = temp;
+            }
+
+            password = new string(NewPassword);
+
             passField.Text = password;
         }
 
@@ -86,7 +122,7 @@ namespace PassGen
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //These are defaults; they should be changed per execution. 
+            //These are defaults; can be changed per user 
             seedField.Text = "DefaultSeed";
             charField.Text = "10";
             nameField.Select();
@@ -95,11 +131,7 @@ namespace PassGen
         private void generateButton_Click(object sender, EventArgs e)
         {
             passField.Text = String.Empty;
-
-            if (CoreName.Length > 0 || Seed.Length > 0 || CharCount.Length > 0)
-            {
-                PassGen(CoreName, Seed, CharCount);
-            }
+            PassGen(CoreName, Seed, CharCount);
         }
 
     }
